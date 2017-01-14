dce1f6830c25b1e188e14222ffa17134b5e1f502
VladGnt <blackvg95@gmail.com>
2017-01-14 21:52:03 +0200

    ADD: project

---
+++ js/script.js
@@ -0,0 +1,20 @@
+/*
+* To Top
+*/
+
+window.onload = function() {
+
+	var scrollUp = document.getElementById('scrollup');
+
+	scrollUp.onclick = function() {
+		window.scrollTo(0,0);
+	};
+
+	window.onscroll = function () {
+		if ( window.pageYOffset > 0 ) {
+			scrollUp.style.display = 'block';
+		} else {
+			scrollUp.style.display = 'none';
+		}
+	};
+};
