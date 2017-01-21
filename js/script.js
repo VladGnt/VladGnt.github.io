740e5ca6213f1db7c64266c37a5b5bbe1ddce92a
VladGnt <blackvg95@gmail.com>
2017-01-21 23:32:38 +0200

    upd

--- js/script.js
+++ js/script.js
@@ -18,3 +18,52 @@ window.onload = function() {
 		}
 	};
 };
+
+
+/*
+ * Owl Carousel
+ */
+$(document).ready(function() {
+	var plugins = {
+		owl: $(".owl-carousel")
+	};
+
+	if (plugins.owl.length) {
+		var i;
+		for (i = 0; i < plugins.owl.length; i++) {
+			var c = $(plugins.owl[i]), responsive = {};
+			var aliaces = ["-", "-xs-", "-sm-", "-md-", "-lg-", "-xl-"], values = [0, 480, 768, 992, 1200, 1800], j, k;
+			for (j = 0; j < values.length; j++) {
+				responsive[values[j]] = {};
+				for (k = j; k >= -1; k--) {
+					if (!responsive[values[j]]["items"] && c.attr("data" + aliaces[k] + "items")) {
+						responsive[values[j]]["items"] = k < 0 ? 1 : parseInt(c.attr("data" + aliaces[k] + "items"));
+					}
+					if (!responsive[values[j]]["stagePadding"] && responsive[values[j]]["stagePadding"] !== 0 && c.attr("data" + aliaces[k] + "stage-padding")) {
+						responsive[values[j]]["stagePadding"] = k < 0 ? 0 : parseInt(c.attr("data" + aliaces[k] + "stage-padding"));
+					}
+					if (!responsive[values[j]]["margin"] && responsive[values[j]]["margin"] !== 0 && c.attr("data" + aliaces[k] + "margin")) {
+						responsive[values[j]]["margin"] = k < 0 ? 30 : parseInt(c.attr("data" + aliaces[k] + "margin"));
+					}
+				}
+			}
+			c.owlCarousel({
+				autoplay: c.attr("data-autoplay") === "true",
+				loop: c.attr("data-loop") !== "false",
+				items: 1,
+				dotsContainer: c.attr("data-pagination-class") || false,
+				navContainer: c.attr("data-navigation-class") || false,
+				mouseDrag: c.attr("data-mouse-drag") !== "false",
+				nav: c.attr("data-nav") === "true",
+				dots: c.attr("data-dots") === "true",
+				dotsEach: c.attr("data-dots-each") ? parseInt(c.attr("data-dots-each")) : false,
+				animateIn: c.attr('data-animation-in') ? c.attr('data-animation-in') : 'slide',
+				animateOut: c.attr('data-animation-out') ? c.attr('data-animation-out') : false,
+				responsive: responsive,
+				navText: []
+			});
+		}
+	}
+});
+
+
